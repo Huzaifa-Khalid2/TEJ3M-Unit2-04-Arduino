0013af2496daac27e7efb703f9d15591e1f30a57
Huzaifa-Khalid2 <74553333+Huzaifa-Khalid2@users.noreply.github.com>
2023-03-21 09:02:35 -0400

    Update light.ino

--- light.ino
+++ light.ino
@@ -1,17 +1,24 @@
-// C++ code
-//
-
-int pin5 = 5;
+/*
+  Created by: Huzaifa Khalid
+  Created on: March 2023
+  Turns the LED on and off for one second repeatedly.
+*/
 
 void setup()
 {
-  pinMode(pin5, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(pin5, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(pin5, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  for (int redvar = 0; redvar <= 1; redvar++) {
+    for (int grnvar = 0; grnvar <= 1; grnvar++) {
+      for (int bluvar = 0; bluvar <= 1; bluvar++) {
+        digitalWrite(13, redvar);
+        digitalWrite(12, grnvar);
+        digitalWrite(11, bluvar);
+        delay(1000); // Wait for 1000 millisecond(s)
+      }
+    }
+  }
 }
